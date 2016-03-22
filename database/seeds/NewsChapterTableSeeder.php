9828724a91b1e0910bfdd6eaf551757799ba57ec
Sergey Donchenko <sergey@severalnines.com>
2016-03-22 18:05:58 +0200

    Добавил сиддер для новостных разделов

---
+++ database/seeds/NewsChapterTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Chapters as Chapter;
+
+class NewsChapterTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Chapter::where('id', 'like', '%%')->delete();
+
+        $faker = Faker\Factory::create('ru_RU');
+
+
+        foreach (range(1,105) as $index) {
+            Chapter::create([
+                'title' => $faker->sentence(10),
+                'description' => $faker->text,
+                'pos' => 0,
+                'is_active' => (int)($index % 2 === 0),
+                'parent_id' => null,
+
+                'type_chapter' => Chapter::TYPE_CHAPTER,
+
+                'created_at' => $faker->dateTime,
+                'updated_at' => $faker->dateTime,
+                'user_id' => 1
+            ]);
+        }
+    }
+}
